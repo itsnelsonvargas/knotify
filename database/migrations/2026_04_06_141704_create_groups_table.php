5210f68349da2e26d1bd6285c4d8f423eb8c8e5c
nelson <itsnelsonvargas@gmail.com>
2026-04-06 22:18:29 +0800

    feat: create a table for grouping the guests

---
+++ database/migrations/2026_04_06_141704_create_groups_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->id();
+            $table->string('group_name'); // e.g., "Vargas Family", "College Friends", "DSWD Team"
+            $table->text('description')->nullable(); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('groups');
+    }
+};
